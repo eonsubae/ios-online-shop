ab08c5e5832a81db9ec704a3f661196d1bdbe7c3
eonsubae <eonsubae@naver.com>
2021-01-10 19:57:40 +0900

    Update Category class

---
+++ Market/Market/Helpers/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Market
+//
+//  Created by KRITSSEAN on 2021/01/10.
+//
+
+import Foundation
+
+// Firebase Headers
+public let kUSER_PATH = "User"
+public let kCATEGORY_PATH = "Category"
+public let kITEMS_PATH = "Items"
+public let kBASKET_PATH = "Basket"
+
+// Category
+public let kNAME = "name"
+public let kIMAGENAME = "imageName"
+public let kOBJECTID = "objectId"
