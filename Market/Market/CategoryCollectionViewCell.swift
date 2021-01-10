717c9e3179be5f31c0ff537e0930548dfcd4fde9
eonsubae <eonsubae@naver.com>
2021-01-10 16:36:13 +0900

    Custom collection view Cell class

---
+++ Market/Market/CategoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Market
+//
+//  Created by KRITSSEAN on 2021/01/10.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
